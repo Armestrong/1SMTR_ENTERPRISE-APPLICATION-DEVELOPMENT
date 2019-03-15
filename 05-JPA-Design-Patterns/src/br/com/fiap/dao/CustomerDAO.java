9db3262e7594145f60fca4d628afe85dba2691ad
Armestrong <Armestrongcalazans@hotmail.com>
2019-03-14 22:01:20 -0300

    REMOVIDO

--- 05-JPA-Design-Patterns/src/br/com/fiap/dao/CustomerDAO.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.dao;
-
-import br.com.fiap.entity.Customer;
-
-public interface CustomerDAO extends GenericDAO<Customer, Integer> {
-
-}
--- 05-JPA-Design-Patterns/src/br/com/fiap/entity/Gender.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.entity;
-
-public enum Gender {
-
-	FEMALE, MALE, OTHERS;
-	
-}
